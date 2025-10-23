8962add4110809b922a8e9c03a287e01ff99551c
Rishi-1482 <rishinath.enugala@gmail.com>
2025-10-23 14:32:49 -0700

    Role1 commit

--- analysis_GTD.R
+++ analysis_GTD.R
@@ -10,3 +10,16 @@ gtd_data <- read.csv("globalterrorismdb_0718dist.csv",
 
 # Basic data exploration
 glimpse(gtd_data)
+View(gtd_data)
+
+unique(gtd_data$country_txt)
+
+gtd_data_clean <- gtd_data %>% 
+  filter(!is.na(country_txt) & country_txt != "NA")
+
+attacks_by_country <- gtd_data_clean %>%
+  group_by(country_txt)
+
+View(attacks_by_country)
+
+
