7068d5e9ac29fcdcf2bec152023f2038977eb426
Ankur Singh <ankur08admin@gmail.com>
2025-10-23 14:22:45 -0700

    Added code to download the datafile

--- analysis_GTD.R
+++ analysis_GTD.R
@@ -4,6 +4,20 @@
 library(dplyr)
 library(ggplot2)
 
+# Download and unzip data file 
+url <- "https://www.kaggle.com/api/v1/datasets/download/START-UMD/gtd"
+destfile <- "data.zip"
+
+# Download the ZIP file
+download.file(url, destfile, mode = "wb")
+
+# Unzip the file to a directory (e.g., 'data')
+unzip(destfile, exdir = getwd())
+
+#Remove the zipfile
+file.remove(destfile)
+
+
 # Load the dataset
 gtd_data <- read.csv("globalterrorismdb_0718dist.csv", 
                      stringsAsFactors = FALSE)
